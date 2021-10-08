283d1c1d93122436265a2c3e7d92c013964877ab
sudo-harshad <hmeshram4@gmail.com>
2021-10-08 10:54:03 +0530

    template added

---
+++ src/main/java/com/atos/SearchFlight/AirTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.atos.SearchFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atos/SearchFlight/AirTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.SearchFlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
